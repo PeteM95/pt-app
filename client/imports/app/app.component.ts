168989709938cb4535884a46796d61c2fcd4d394
Peter Milionis <pete.milionis@gmail.com>
2017-05-24 21:34:27 -0700

    before wiping to start with Angular instead of Meteor

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
-import { StatusBar, Splashscreen } from 'ionic-angular';
+import { StatusBar, Splashscreen } from 'ionic-native';
+
+import { MapsComponent } from '../pages/maps/maps';
 import template from "./app.html";
 
 @Component({
@@ -8,7 +10,11 @@ import template from "./app.html";
 	template
 })
 export class App {
+	rootPage: any;
+
 	constructor(platform: Platform) {
+		this.rootPage = MapsComponent;
+
 		platform.ready().then(() => {
 			// Okay, so the platform is ready and our plugins are available.
 			// Here you can do any higher level native things you might need.
--- client/imports/app/app.module.ts
+++ client/imports/app/app.module.ts
@@ -1,17 +1,20 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { App } from './app.component';
+import { MapsComponent } from '../pages/maps/maps';
 
 @NgModule({
 	declarations: [
-		App
+		App,
+		MapsComponent
 	],
 	imports: [
 		IonicModule.forRoot(App),
 	],
 	bootstrap: [IonicApp],
 	entryComponents: [
-		App
+		App,
+		MapsComponent
 	],
 	providers: [
 		{ provide: ErrorHandler, useClass: IonicErrorHandler }
